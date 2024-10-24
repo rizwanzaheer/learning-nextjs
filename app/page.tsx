df2147f588114896124afcf8c21c17897cfcb2ea
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2024-10-24 15:43:00 +0500

    Fix bugs after upgrade (#3)
    * removed pnpm and used bun
    upgrade to next 14.4
    
    * Upgrade to v15
    
    * fix bug's/concerns after upgrade to v15

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,25 @@ export const metadata: Metadata = {
   description: "Visual metadata home page description.",
 };
 
-export default async function Page({
-  children,
-  analytics,
-  team,
-}: {
-  children: React.ReactNode;
-  analytics: React.ReactNode;
-  team: React.ReactNode;
-}) {
+export default async function Page() {
+//   {
+//   children,
+// }: // analytics,
+// // team,
+// {
+//   children: React.ReactNode;
+//   // analytics: React.ReactNode;
+//   // team: React.ReactNode;
+// }
   // const res = await fetch("http://localhost:3000/api/"); // this will create error boundary stuff
-  const res = await fetch(`${process.env.BASE_URL}/api/v1`);
-  console.log("res.ok is: ", res.ok);
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  // const data = await res.json();
-  console.log("res is: ", res);
+  // const res = await fetch(`${process.env.BASE_URL}/api/v1`);
+  // console.log("res.ok is: ", res.ok);
+  // if (!res.ok) {
+  //   // This will activate the closest `error.js` Error Boundary
+  //   throw new Error("Failed to fetch data");
+  // }
+  // // const data = await res.json();
+  // console.log("res is: ", res);
   return (
     <div>
       <h1>Hello, Next.js 13 upgrade!</h1>
@@ -45,8 +46,8 @@ export default async function Page({
       <br />
 
       <Link href="/error/should/show">to global not found</Link>
-      {analytics}
-      {team}
+      {/* {analytics}
+      {team} */}
 
       <div style={{ width: "100%", height: "200vh", backgroundColor: "gray" }}>
         <h1>this is style div</h1>
