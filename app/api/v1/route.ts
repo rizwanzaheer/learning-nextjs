78875ae4ed77a5ad3d4418c170a4afdd103282f5
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2024-10-24 15:54:59 +0500

    using await cookies

--- app/api/v1/route.ts
+++ app/api/v1/route.ts
@@ -1,12 +1,14 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-
-
 export async function GET(request: NextRequest) {
-  const token = request.cookies.get("token") || request.cookies.set("token", "localTokenTesting").get("token");
-  console.log("token is: ", token);
+  // const token = request.cookies.get("token") || request.cookies.set("token", "localTokenTesting").get("token");
+  const cookieStore = await cookies();
+  const token =
+    cookieStore.get("token") ||
+    cookieStore.set("token", "localTokenTesting").get("token");
 
+  console.log("token is: ", token);
 
   return NextResponse.json({ data: token });
 
